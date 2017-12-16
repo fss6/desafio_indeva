eada55a3dbefaf82872d6d5e6c13341742010d39
Fábio Santos <fabiosantos.pe@gmail.com>
2017-12-15 21:43:53 -0300

    - Atividade 002: layout da aplicaçao * Adicao dos assets e configuração básica para o tema adminLTE * Troca da gem turbolinks por jquery-tubolinks para resolver problema de reload de assets * Generate Homecontroller e sua respectiva rota

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
-//= require turbolinks
+//= require jquery.turbolinks
+//= require bootstrap.min
+//= require adminlte.min
 //= require_tree .
+
