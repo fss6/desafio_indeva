be385c28341497f0ef29bb919be405e6d57b1474
Fábio Santos <fabiosantos.pe@gmail.com>
2017-12-21 23:49:36 -0300

    - Atividade 011: Tests model Seller

--- test/models/seller_test.rb
+++ test/models/seller_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class SellerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "validate name presence" do
+    assert_not Seller.new({
+      owner_id: Owner.first.id,
+      registration: '12332'
+    }).save
+  end
+
+  test "validate registration presence" do
+    assert_not Seller.new({
+      owner_id: Owner.first.id,
+      name: 'MyName'
+    }).save
+  end
+
+  test "validate owner_id presence" do
+    assert_not Seller.new({
+      name: 'MyName',
+      registration: '12332'
+    }).save
+  end
+
+  test "validate registration uniqueness" do
+    assert_not Seller.new({
+      name: 'MyName',
+      owner_id: Owner.first.id,
+      registration: '12345'
+    }).save
+  end
+
+  test "validate registration numericality" do
+    assert_not Seller.new({
+      name: 'MyName',
+      owner_id: Owner.first.id,
+      registration: 'B12332'
+    }).save
+  end
 end
